6cf2816fcf47f32f129e3c8fc6af980756fe0511
Ahmed Saeed <asaeed1104@gmail.com>
2024-06-17 09:20:55 -0400

    add valid input test

--- cypress/pageObjects/mortgageCalculatorPage.js
+++ cypress/pageObjects/mortgageCalculatorPage.js
@@ -2,7 +2,7 @@
 class MortgageCalculator {
 
     loanProgramLabel = "//label[text()='Loan program']";
-    interestRateLable = "//label[text()='Interest rate']";
+    interestRateLabel = "//label[text()='Interest rate']";
     advancedButton = "//button[text()='Advanced']";
     moreInfoButton = "//button/span[contains(text(),'Interest rate')]";
     seeCurrentRateButton = "//a[text()='See current rates']";
@@ -10,6 +10,7 @@ class MortgageCalculator {
     moreInfoPopup = "//section//h4[text()='Interest rate']"
     moreInfoClose = "button[class^='CloseButton']"
     invalidMessage = "//p[contains(text(),'not a valid number')]";
+    pIvalue = "(//*[@class='arc-label-value'])";
 
     visit() {
         cy.visit("https://www.zillow.com/mortgage-calculator/");
@@ -18,6 +19,7 @@ class MortgageCalculator {
     checkTooltipForInterestRate() {
         // check info button is visible and click it
         cy.xpath(this.moreInfoButton)
+        .scrollIntoView()
         .should('be.visible')
         .click({force: true});
 
@@ -27,7 +29,7 @@ class MortgageCalculator {
         cy.xpath(this.moreInfoPopup).should('not.exist');
     };
 
-    clickCurrentRateButton(currentUrl,rateUrl) {
+    clickCurrentRateButton(currentUrl, rateUrl) {
         //check current rate button is visible and click it
         cy.xpath(this.seeCurrentRateButton)
         .should('be.visible')
@@ -55,16 +57,45 @@ class MortgageCalculator {
     };
 
     verifyInvalidInputValues(value) {
+
         // locate field, clear it, type special characters, 
         // click outside field, verify invalid message.
         cy.get(this.rateInputField)
         .should('be.visible')
         .clear()
+        .should('have.value','')
         .type(value).tab()
-   //     .click();
         cy.xpath(this.invalidMessage).should('be.visible');      
     };
 
+    verifyValidInputValues() {
+
+        // Get the value from the input field and verify it exists
+        cy.get(this.rateInputField).invoke('val').then((interestRateValue) => {
+            cy.xpath(this.pIvalue).invoke('text').then((initialPrincipalInterestValue) => {
+            expect(interestRateValue).to.exist;
+            expect(interestRateValue).to.not.be.empty;
+
+            // Clear the input field and verify
+            cy.get(this.rateInputField).clear().tab();
+            cy.get(this.rateInputField).should('have.value', '');
+
+            // Input a different value and wait so the value updates.
+            cy.get(this.rateInputField).type('4.25').tab();
+            cy.wait(2000);
+
+            // Check that the new value
+            cy.get(this.rateInputField).should('have.value', '4.25');
+
+                // Verify that the principal & interest field updates
+                cy.xpath(this.pIvalue).invoke('text').then((updatedPrincipalInterestValue) => {
+
+                // Assert the updated values are as expected
+                expect(updatedPrincipalInterestValue).to.not.equal(initialPrincipalInterestValue);
+                });
+            });
+        });
+    }
 }
 
 export default MortgageCalculator;
